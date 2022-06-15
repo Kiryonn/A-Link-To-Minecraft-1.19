688fcb1132d49fe5f8342d01269761aa4f8258a3
Kiryonn <joshmachu@gmail.com>
2022-06-15 21:21:51 +0200

    first commit

---
+++ src/main/java/net/kiryonn/alinktominecraft/ALinkToMinecraftClient.java
@@ -0,0 +1,11 @@
+package net.kiryonn.alinktominecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ALinkToMinecraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
