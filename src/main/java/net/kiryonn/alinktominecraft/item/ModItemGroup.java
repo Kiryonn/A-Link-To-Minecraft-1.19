6f2bf3d33318f8ff7f2dccd2c79f9b395550c025
Kiryonn <joshmachu@gmail.com>
2022-06-17 23:16:06 +0200

    added french translation

--- src/main/java/net/kiryonn/alinktominecraft/item/ModItemGroup.java
+++ src/main/java/net/kiryonn/alinktominecraft/item/ModItemGroup.java
@@ -4,10 +4,11 @@ import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.kiryonn.alinktominecraft.ALinkToMinecraft;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 public class ModItemGroup {
     public static final ItemGroup A_LINK_TO_MINECRAFT = FabricItemGroupBuilder.build(
-            new Identifier(ALinkToMinecraft.MOD_ID),
-            ()->new ItemStack(ModItems.BLUE_EMERALD));
+            new Identifier(ALinkToMinecraft.MOD_ID, "itemgroup"),
+            ()->new ItemStack(Items.EMERALD));
 }
